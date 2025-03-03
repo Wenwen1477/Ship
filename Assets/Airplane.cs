b024166fd68e7db01b806217a7b6eb6f5baf988f
Wenwen1477 <163106315+Wenwen1477@users.noreply.github.com>
2025-03-03 14:42:30 +0700

    V.3

--- Assets/Airplane.cs
+++ Assets/Airplane.cs
@@ -2,21 +2,61 @@ using UnityEngine;
 
 public class Airplane : MonoBehaviour
 {
-    public class Airplane : MonoBehaviour
+    public float thrustPower = 500f;
+    public float liftBooster = 5f;
+    public float drag = 0.02f;
+    public float angularDrag = 0.01f;
+
+    [SerializeField] float yawPower = 50f;  // Turning speed
+    [SerializeField] float pitchPower = 50f; // Nose up/down speed
+    [SerializeField] float rollPower = 30f;
+
+    private Rigidbody rb;
+
+    void Start()
     {
-        [SerializeField] Rigidbody rb;
-        [SerializeField] float enginePower = 20f;
-        [SerializeField] float liftBooster = 0.5f;
-        [SerializeField] float drag = 0.001f;
-        [SerializeField] float angularDrag = 0.001f;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
 
-        private void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-        }
+    void FixedUpdate()
+    {
+        ApplyThrust();
+        ApplyLift();
+        ApplyDrag();
+        HandleRotation();
+    }
 
-        void FixedUpdate()
+    void ApplyThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
         {
-            // Logic for airplane physics will go here
+            rb.AddForce(transform.forward * thrustPower, ForceMode.Acceleration);
         }
     }
+
+    void ApplyLift()
+    {
+        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+    }
+
+    void ApplyDrag()
+    {
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+    }
+
+    void HandleRotation()
+    {
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up * yaw);    // Yaw (Turn left/right)
+        rb.AddTorque(transform.right * pitch); // Pitch (Nose up/down)
+        rb.AddTorque(transform.forward * roll); // Roll (Tilting)
+    }
+}
